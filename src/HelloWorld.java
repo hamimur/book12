63c72e2132cc26bb0c9350e0e6e9e42fd92b2d87
skatta3 <skatta3@yahoo.com>
2020-05-05 20:09:47 +0530

    My First Commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,22 @@
+
+
+public class HelloWorld {
+	// int is the data type, Number1 and Number2 are Variable names, ; is the end of the statement
+	int Number1;
+	String ProjectName;
+	int Number2;
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Java Training");
+		
+		// Instantiating HelloWorld class
+		HelloWorld helloObject = new HelloWorld();
+		
+		// Printing Class Variables
+		System.out.println("Number1...." + helloObject.Number1);
+		System.out.println("Number2...." + helloObject.Number2);
+		
+		
+	}
+
+}
